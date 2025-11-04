3e4925e7efaad2b5f8e26c99983c920e9714d5ca
Patvath88 <patvath8@gmail.com>
2025-11-04 14:50:04 -0500

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
-def sanitize_dataframe_for_streamlit(df: pd.DataFrame) -> pd.DataFrame:
-    """Cleans DataFrame for Streamlit (fixes Arrow conversion issues)."""
-    if df is None or df.empty:
-        return df
-    df = df.copy()
+def sanitize_dataframe_for_streamlit(df: pd.DataFrame):
     for col in df.columns:
         if pd.api.types.is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.strftime("%Y-%m-%d")
-        elif pd.api.types.is_object_dtype(df[col]):
             df[col] = df[col].astype(str)
     return df
